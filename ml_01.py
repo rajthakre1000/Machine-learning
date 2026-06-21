3ba759a1cfaa313d0d24d864633748f43b8dc05b
rajthakre1000 <rajkumarthakre633@gmail.com>
2026-06-21 17:59:13 +0530

    ml_02.py

---
+++ ml_01.py
@@ -0,0 +1,16 @@
+#AI vs ML vs DS
+#Expert system: (use many if else conditions by any expert) ex. chess 
+# Knowledge base > inference engine > user interface
+
+# ML (not explicite programming (dont write rule for each condition))
+# find pattern in data and and predict output
+# system generate rules
+
+# Deep learning 
+# after 2012  peak
+# same pattern as ML
+# additional : diffrent algorithm(like neurons)
+# certain things ML can't do, we have to provide data in ml
+# while in DL it automatically create diffrence (like diff btw dog and cat) we dont have to decalre diffrences
+# adding more layer improves efficeny/ prediction power
+#* while giving, at a point ML will stabalise but DL will keep impoving
---
+++ ml_02.py
@@ -0,0 +1,24 @@
+#Types of ML
+#Supervise Learning: 
+#relationship btw two input and output to pridict next output
+            # data : 1. numerical(age, wt), 2. Categerical (gender, nation)
+# Regression machine: if output is numerical(number)
+# Classifiaction machine: if output is Categerical (ex. dog or cat pridiction)
+
+
+#Unsupervise Learning:
+#Only input is present
+# 1. clustering :  used to group similar data points together into clusters without any pre-existing labels
+
+# 2. dimensionality reductiona: technique that reduces the number of features in a 
+# dataset by transforming high-dimensional data into a lower-dimensional space while preserving essential information
+
+# 3. Anomaly detection is the process of identifying data points, events, or patterns
+#  that are unusual and deviate significantly from the norm (ex. point outside the range )
+
+#Semi-supervise Learning:
+# Lable one or two data labeled and the other will automatically identify
+
+# Reinforcemt learning
+# a type of machine learning where an "agent" learns to make decisions through
+#  trial and error by interacting with an environment to achieve a goal.
